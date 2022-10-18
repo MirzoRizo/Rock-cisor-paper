05cd3e5571ed8d175e6452a24a2eb2367613bee8
MirzoRizo <mirzojuma2002@gmail.com>
2022-10-18 22:56:46 +0300

    The last release

--- script.js
+++ script.js
@@ -11,21 +11,42 @@ function getComputerСhoise() {
   return rps;
 }
 
-const playerSelection = prompt("Start the game!").toLowerCase();
-const computerSelection = getComputerСhoise();
-console.log(computerSelection);
+let comp_score = 0;
+let player_score = 0;
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+  const playerSelection = prompt("Start the game!").toLowerCase();
+  const computerSelection = getComputerСhoise();
   if (computerSelection == playerSelection) {
-    return "Tie game!";
+    alert ("Tie game!");
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
-    return "You Lose";
+    comp_score += 1;
+    alert ("You Lose");
   } else {
-    return "You Win";
+    player_score += 1;
+    alert ("You Win");
   }
 }
-console.log(playRound(playerSelection, computerSelection));
+
+function WhoWon() {
+  if (comp_score == player_score) {
+    alert ("Draw");
+  } else if (comp_score > player_score) {
+    alert (`You lose to computer ${comp_score} by ${player_score}`);
+  } else if (comp_score < player_score) {
+    alert (`You win computer ${player_score} by ${comp_score}`);
+  }
+}
+
+function Game() {
+  while (!(comp_score >= 5 || player_score >= 5)) {
+    console.log(playRound());
+  }
+  console.log(WhoWon());
+}
+
+Game();

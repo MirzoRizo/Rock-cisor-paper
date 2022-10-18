63ccccf9af054ec9adfc8aa6a8fcbf6c1b745a60
MirzoRizo <mirzojuma2002@gmail.com>
2022-10-18 22:07:34 +0300

    It start to work

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hallo")
+function getComputerСhoise() {
+  const rand_num = Math.floor(Math.random() * 3) + 1;
+  let rps = "";
+  if (rand_num === 1) {
+    rps = "rock";
+  } else if (rand_num === 2) {
+    rps = "paper";
+  } else if (rand_num === 3) {
+    rps = "scissors";
+  }
+  return rps;
+}
+
+const playerSelection = prompt("Start the game!").toLowerCase();
+const computerSelection = getComputerСhoise();
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection == playerSelection) {
+    return "Tie game!";
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ) {
+    return "You Lose";
+  } else {
+    return "You Win";
+  }
+}
+console.log(playRound(playerSelection, computerSelection));

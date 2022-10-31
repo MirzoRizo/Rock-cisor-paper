a932d5870aa363053bc98aaad06c681049ee22a5
MirzoRizo <mirzojuma2002@gmail.com>
2022-10-31 16:20:30 +0300

    gui

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let comp_score = 0;
+let player_score = 0;
+const buttons = document.querySelectorAll('input')
+
+function disableButtons() {
+  buttons.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
+
 function getComputerСhoise() {
   const rand_num = Math.floor(Math.random() * 3) + 1;
   let rps = "";
@@ -11,42 +21,71 @@ function getComputerСhoise() {
   return rps;
 }
 
-let comp_score = 0;
-let player_score = 0;
-
-function playRound() {
-  const playerSelection = prompt("Start the game!").toLowerCase();
+function playRound(choose) {
+  const playerSelection = choose;
   const computerSelection = getComputerСhoise();
+  let result = "";
+
   if (computerSelection == playerSelection) {
-    alert ("Tie game!");
+    result =
+      "It's a tie. You both chose " +
+      playerSelection +
+      "<br><br>Player score: " +
+      player_score +
+      "<br>Computer score: " +
+      comp_score;
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
     comp_score += 1;
-    alert ("You Lose");
+    result =
+      "You lose! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      "<br><br>Player score: " +
+      player_score +
+      "<br>Computer score: " +
+      comp_score;
+    if (comp_score == 5) {
+      result += "<br><br>I won the game! Reload the page to play again";
+      disableButtons();
+    }
   } else {
     player_score += 1;
-    alert ("You Win");
+    result =
+      "You win! " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      "<br><br>Player score: " +
+      player_score +
+      "<br>Computer score: " +
+      comp_score;
+
+    if (player_score == 5) {
+      result += "<br><br>You won the game! Reload the page to play again";
+      disableButtons();
+    }
   }
+
+  document.getElementById("result").innerHTML = result;
+  return;
 }
 
-function WhoWon() {
-  if (comp_score == player_score) {
-    alert ("Draw");
-  } else if (comp_score > player_score) {
-    alert (`You lose to computer ${comp_score} by ${player_score}`);
-  } else if (comp_score < player_score) {
-    alert (`You win computer ${player_score} by ${comp_score}`);
-  }
-}
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", () => {
+  playRound("rock");
+});
 
-function Game() {
-  while (!(comp_score >= 5 || player_score >= 5)) {
-    console.log(playRound());
-  }
-  console.log(WhoWon());
-}
+const btnScissors = document.querySelector("#scissors");
+btnScissors.addEventListener("click", () => {
+  playRound("scissors");
+});
 
-Game();
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", () => {
+  playRound("paper");
+});
